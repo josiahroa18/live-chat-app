3999759039095db8d72f885ec942543cc86f3c31
Josiah Roa <josiahroa18@gmail.com>
2020-09-11 00:23:26 -0600

    Updated server

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
-const http = require('http').createServer();
-const io = require('socket.io')(http);
+const PORT = process.env.PORT || 5000;
+const http = require('http');
 const path = require('path');
 
+const server = http.createServer(app)
+const io = require('socket.io')(server);
+
+
 // Middleware to serve the react client
-app.use(express.static(path.join(__dirname, 'client/build')));
 if(process.env.NODE_ENV == 'production'){
-    app.use(express.static('client/build/index.html'))
+    app.use(express.static('client/build'))
+
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname = 'client/build/index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     })
 }
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/public/index.html'));
-})
 
 io.on('connection', socket => {
     socket.on('message', message => {
@@ -23,4 +23,6 @@ io.on('connection', socket => {
     })
 })
 
-http.listen(port);
+server.listen(PORT, () => {
+    console.log(`Server is starting at PORT: ${PORT}`);
+});
