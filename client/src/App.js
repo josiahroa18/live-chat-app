b1b289a472e9197b629a046b15c778ebacac8d2d
Josiah Roa <josiahroa18@gmail.com>
2020-09-11 13:29:47 -0600

    Added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,65 @@
 import React from 'react';
+import { Route, useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Messenger from './pages/Messenger';
+import Card from '@material-ui/core/Card';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 import './App.css';
 
+const useStyles = makeStyles({
+  root: {
+    width: '600px',
+    margin: '0 auto',
+    padding: '20px',
+    backgroundColor: '#f5f5f5'
+  },
+  inputField: {
+    width: '100%'
+  },
+  wrapper: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  button: {
+    margin: '20px'
+  }
+})
+
 function App() {
+  const classes = useStyles();
+  const history = useHistory();
+
   return (
     <div className="App">
-      <div style={{ backgroundColor: '#f5f5f5', padding: '2px' }}>
-        <h1>Live Chat</h1>
-      </div>
-      <div style={{ height: `600px`, overflowY: 'auto', border: '1px solid black' }}>
-
-      </div>
-      <div className='tool-bar'>
-        <div style={{ width: '75%' }}>
-          <TextField
-            fullWidth
-            placeholder='Enter your message here'
-            variant="outlined"
-          />
+      <Route exact path='/'>
+        <div>
+          <h1>Welcome to Live Chat!</h1>
+          <Card className={classes.root}>
+            <TextField id="outlined-basic" label="Display Name" variant="outlined" className={classes.inputField}/>
+            <div className={classes.wrapper}>
+              <Button 
+                  variant="contained" 
+                  color='primary'
+                  className={classes.button}
+                  onClick={() => {
+                    history.push(`/messenger?displayName=USER&roomId=myRoom`)
+                  }}
+              >Create Room</Button>
+              <Button 
+                  variant="contained" 
+                  color='primary'
+                  className={classes.button}
+              >Join Room</Button>
+            </div>
+          </Card>
         </div>
-        <div style={{ width: '20%' }}>
-          <Button 
-            fullWidth 
-            variant="contained" 
-            color='primary'
-          >Send</Button>
-        </div>
-      </div>
+      </Route>
+      <Route path='/messenger'>
+        <Messenger/>
+      </Route>
     </div>
   );
 }
